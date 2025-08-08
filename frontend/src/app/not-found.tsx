de05fb344a4f0029e8dde9e70d98592d208a5275
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-08 22:12:55 +0530

    fix the build error

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function NotFound() {
   const pathname = usePathname();
@@ -15,9 +16,9 @@ export default function NotFound() {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <Link  href="/" className="text-blue-500 hover:text-blue-700 underline">
+        <Link href="/" className="text-blue-500 hover:text-blue-700 underline">
           Return to Home
-        </Link >
+        </Link>
       </div>
     </div>
   );
